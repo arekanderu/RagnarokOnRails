8cc9c0c0def9e2c3b281f7946702760e0002f109
Alexander <afernandez36@academic.rrc.ca>
2019-11-18 16:45:42 -0600

    Supplied Data in Province and Customer table

--- app/admin/provinces.rb
+++ app/admin/provinces.rb
@@ -5,8 +5,8 @@ ActiveAdmin.register Province do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :acronym, :gst, :pst, :hst
-  #
+  permit_params :name, :acronym, :gst, :pst, :hst
+
   # or
   #
   # permit_params do
@@ -14,5 +14,5 @@ ActiveAdmin.register Province do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
