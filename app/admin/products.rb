11c7dec0a9bf54f7753c21e173c6c113404674b0
Alexander <afernandez36@academic.rrc.ca>
2019-11-18 16:34:27 -0600

    Order active admin

---
+++ app/admin/products.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Product do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :category_id, :number_in_stock, :attack, :magic_attack, :description, :price
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :category_id, :number_in_stock, :attack, :magic_attack, :description, :price]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
