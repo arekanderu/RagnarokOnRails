8bbccb750ae74e47e3449eedeb1692248f478ea9
Alexander <afernandez36@academic.rrc.ca>
2019-11-18 16:33:45 -0600

    Order active admin

---
+++ app/admin/orders.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Order do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :order_number, :customer_id, :quantity, :sub_total, :tax_rate, :total_price
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:order_number, :customer_id, :quantity, :sub_total, :tax_rate, :total_price]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
