e42fe8f98e6e0783ab885f780cf83b2e527cf302
Alexander <afernandez36@academic.rrc.ca>
2019-11-19 10:32:37 -0600

    Search working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,28 +1,31 @@
+# frozen_string_literal: true
+
 class ProductsController < ApplicationController
   def index
     @products =
 
-    if params[:product] && params[:product][:category_id]
-      Product.search(params[:product][:category_id])
+      if params[:product] && params[:product][:category_id]
+        Product.search(params[:product][:category_id])
 
-    elsif params[:option] == 'New'
-      Product.order('created_at DESC, category_id')
+      elsif params[:option] == 'New'
+        Product.order('created_at DESC, category_id')
 
-    elsif params[:option] == 'Updated'
-      Product.order('updated_at DESC, category_id')
+      elsif params[:option] == 'Updated'
+        Product.order('updated_at DESC, category_id')
 
-    else
-      Product.all
+      else
+        Product.all
+      end
+
+    @search = params['search']
+    @category = params['category_id']
+    if @search.present?
+      @name = @search['name']['category_id']
+      @category = @search['category_id']
+      @products = Product.where('name LIKE ?', "%#{@name}%")
     end
 
-    # @search = params['search']
-    # if @search.present?
-    #   @name = @search['name']
-    #   @products = Product.where('name LIKE ?', "%#{@name}%")
-    # end
-    @search_results = @products.paginate(page: params[:page], per_page: 5)
-
-
+    @paginate_results = @products.paginate(page: params[:page], per_page: 5)
   end
 
   def show
@@ -31,6 +34,7 @@ class ProductsController < ApplicationController
 
   def search_results
     @query = params[:query]
-    Product.where('name LIKE ?', "%#{@query}%")
+    @products = Product.where('name LIKE ?', "%#{@query}%")
+
   end
 end
