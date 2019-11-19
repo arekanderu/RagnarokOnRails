d3b4d6049dc061cda56c9c6e87b85de272c2bc2f
Alexander <afernandez36@academic.rrc.ca>
2019-11-18 19:53:00 -0600

    Added filter New and Updated

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,16 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products =
+
+    if params[:option] == 'New'
+      Product.order('created_at DESC, category_id')
+
+    elsif params[:option] == 'Updated'
+      Product.order('updated_at DESC, category_id')
+
+    else
+      Product.all
+    end
   end
 
   def show
