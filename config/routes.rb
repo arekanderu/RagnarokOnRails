90f234773a0a5df01977a983e1de5a551cf21c91
Alexander <afernandez36@academic.rrc.ca>
2019-11-18 19:30:28 -0600

    Fixed routes and home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'products/index'
-  get 'products/show'
   resources :categories, only: %i[index show]
+  resources :products, only: %i[index show]
+
+  root 'products#index'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
