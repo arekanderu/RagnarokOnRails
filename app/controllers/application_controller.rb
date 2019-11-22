26f48fe85690feda3152e7b5b839ff33d7fb804a
Alexander <afernandez36@academic.rrc.ca>
2019-11-22 00:11:10 -0600

    Devise fully functional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def flash_class(level)
     case level
@@ -9,5 +10,13 @@ class ApplicationController < ActionController::Base
     when :error then 'alert alert-error'
     when :alert then 'alert alert-error'
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :address, :province, :postal_code, :city, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:login, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :address, :province, :postal_code, :city, :password, :password_confirmation, :current_password) }
   end
 end
